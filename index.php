fa203d289394215ec9d650fdd170135e3d85aa3b
sakyamous <sakyamous@gmail.com>
2021-12-19 23:09:10 +0700

    add new file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas MPDF</title>
+</head>
+<body>
+<h1>Halaman Manajemen Data Mahasiswa</h1>
+<h4>Menu</h4>
+<ul>
+<li><a href="prnMhsPdf.php" alt="" target="_blank">Data Mahasiswa Sistem Informasi</a></li>
+</ul>
+    
+</body>
+</html>
